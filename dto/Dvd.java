30aa4c2020674a348c194cb660da9326d6c772f5
urvaxirani <88558812+urvaxirani@users.noreply.github.com>
2021-10-30 17:34:48 -0400

    Add files via upload

---
+++ dto/Dvd.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.dvdlibrary.dto;
+
+/**
+ *
+ * @author urvax
+ */
+public class Dvd {
+    private String title;
+    private String releaseDate;
+    private String mpaaRating;
+    private String directorsName;
+    private String studio;
+    private String additionalInfo;
+
+    public Dvd(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirectorsName() {
+        return directorsName;
+    }
+
+    public void setDirectorsName(String directorsName) {
+        this.directorsName = directorsName;
+    }
+    
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+    
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+}
